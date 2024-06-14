bcab90dabd0f7c2831f8cb5b5620d7de6dfe2174
Keny <160526949+KeniaLoreleySalazar01@users.noreply.github.com>
2024-06-14 02:03:38 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Nombre: Kenia Loreley Salazar Mejía</p>'; ?> 
+    <?php echo '<p>colonia: wuevito Frito</p>'; ?> 
+    <?php echo '<p>municipio: Oaxaca</p>'; ?> 
+    <?php echo '<p>telefono: 844 850 3406</p>'; ?> 
+</html>
